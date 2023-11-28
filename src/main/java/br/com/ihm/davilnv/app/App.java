5b77ed35d820077a9e9c47b71d064573fc74917e
davilnv <davilima871@gmail.com>
2023-11-28 12:14:30 -0300

    Create the mp3 player and config panel

--- src/main/java/br/com/ihm/davilnv/app/App.java
+++ src/main/java/br/com/ihm/davilnv/app/App.java
@@ -1,11 +1,12 @@
 package br.com.ihm.davilnv.app;
 
 import br.com.ihm.davilnv.controller.GameController;
+import javazoom.jl.decoder.JavaLayerException;
 
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, JavaLayerException {
         new GameController();
     }
 }
