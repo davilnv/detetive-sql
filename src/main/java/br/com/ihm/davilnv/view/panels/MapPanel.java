68f28cee8a629975f3d2f607a141b2686b13d7a7
davilnv <davilima871@gmail.com>
2024-02-08 09:38:37 -0300

    Revert "Feat: Keyboard alert for NPC and Computer"
    This reverts commit fb32254b318135d1cba2486a0c210a7183ea8eba.

--- src/main/java/br/com/ihm/davilnv/view/panels/MapPanel.java
+++ src/main/java/br/com/ihm/davilnv/view/panels/MapPanel.java
@@ -4,14 +4,11 @@ import br.com.ihm.davilnv.controller.GameController;
 import br.com.ihm.davilnv.model.*;
 import br.com.ihm.davilnv.view.components.DialogBox;
 import br.com.ihm.davilnv.view.components.Phone;
-import br.com.ihm.davilnv.view.frames.BaseFrame;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -24,19 +21,6 @@ public class MapPanel extends BasePanel {
     private Phone phone;
 
     private static final double ZOOM_LEVEL = 2.0;
-    private static final int RECT_X_OFFSET = 64;
-    private static final int RECT_Y_OFFSET = 15;
-    private static final int RECT_WIDTH = 312;
-    private static final int RECT_HEIGHT = 50;
-    private static final int FONT_SIZE = 8;
-    private static final int TEXT_X_OFFSET = 74;
-    private static final int TEXT_Y_OFFSET = 30;
-    private static final int TEXT_MAX_WIDTH = 185;
-
-    private static final int FRAMES_PER_SECOND = 20;
-    private int frameCounter = 0;
-    private boolean isActive;
-    private ImageIcon enterKeyIcon;
 
     public MapPanel(String key) {
         super(key);
@@ -46,8 +30,9 @@ public class MapPanel extends BasePanel {
     }
 
     public void createOffscreenImage() {
+        // Create an off-screen image for double buffering
+//		offscreenImage = createImage(getWidth(), getHeight());
         offscreenImage = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
-        enterKeyIcon = new ImageIcon(Objects.requireNonNull(DialogBox.class.getResource("/assets/images/icons/enter-keyboard-50-perc.png")));
     }
 
     @Override
@@ -79,11 +64,6 @@ public class MapPanel extends BasePanel {
         g2d.drawImage(logic.getLayer("top").layerImage, 0, 0, null);
         g2d.drawImage(logic.getLayer("front-top").layerImage, 0, 0, null);
 
-        NPC nearbyNPC = player.getNearbyNPC(logic.getNpcs());
-
-        if (player.getNearbyComputer(logic.getComputer()) || nearbyNPC != null) {
-            drawKeyboardWarning(g2d, player.getX(), player.getY());
-        }
 
         if (dialogBox.isVisible()) {
             dialogBox.draw(g2d, player.getNearbyNPC().getX(), player.getNearbyNPC().getY());
@@ -103,20 +83,6 @@ public class MapPanel extends BasePanel {
 
         // Draw the off-screen image to the screen
         g.drawImage(offscreenImage, 0, 0, this);
-    }
-
-    private void drawKeyboardWarning(Graphics2D g2d, int x, int y) {
-        g2d.setFont(BaseFrame.getFont(FONT_SIZE));
-        frameCounter++;
-        if (frameCounter >= FRAMES_PER_SECOND) {
-            frameCounter = 0;
-            isActive = !isActive;
-        }
-
-        if (isActive) {
-            g2d.drawString("Para interagir pressione", x + TEXT_X_OFFSET, y + RECT_HEIGHT + 10);
-            g2d.drawImage(enterKeyIcon.getImage(), x + RECT_X_OFFSET + 118, y + RECT_HEIGHT + 3, null);
-        }
     }
 
     private void showColisionRectangle(Graphics2D g) {
